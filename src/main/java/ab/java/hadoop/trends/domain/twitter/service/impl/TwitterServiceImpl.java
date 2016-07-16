6e3eab1e267f6d8674abfa599d08331d4e686a9d
adrian83 <adrianbrzoza1983@gmail.com>
2016-07-16 14:06:21 +0200

    twitter client fixed

--- src/main/java/ab/java/hadoop/trends/domain/twitter/service/impl/TwitterServiceImpl.java
+++ src/main/java/ab/java/hadoop/trends/domain/twitter/service/impl/TwitterServiceImpl.java
@@ -15,6 +15,7 @@ import com.twitter.hbc.core.endpoint.StatusesSampleEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.BasicClient;
 import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
 
 import ab.java.hadoop.trends.config.TwitterConfig;
 import ab.java.hadoop.trends.domain.twitter.service.TwitterService;
@@ -32,79 +33,52 @@ public class TwitterServiceImpl implements TwitterService {
 
 	@Override
 	public void getTwitts() {
+
 		BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
 
-	    // Define our endpoint: By default, delimited=length is set (we need this for our processor)
-	    // and stall warnings are on.
-	    StatusesSampleEndpoint endpoint = new StatusesSampleEndpoint();
-	    endpoint.stallWarnings(false);
+		// Define our endpoint: By default, delimited=length is set (we need
+		// this for our processor)
+		// and stall warnings are on.
+		StatusesSampleEndpoint endpoint = new StatusesSampleEndpoint();
+		endpoint.stallWarnings(false);
 
-	    Authentication auth = config.getAuhentication();
-	    //Authentication auth = new com.twitter.hbc.httpclient.auth.BasicAuth(username, password);
+		Authentication auth = config.getAuhentication();
+		// Authentication auth = new
+		// com.twitter.hbc.httpclient.auth.BasicAuth(username, password);
 
-	    // Create a new BasicClient. By default gzip is enabled.
-	    BasicClient client = new ClientBuilder()
-	            .name("client")
-	            .hosts(Constants.STREAM_HOST)
-	            .endpoint(endpoint)
-	            .authentication(auth)
-	            .processor(new StringDelimitedProcessor(queue))
-	            .build();
+		// Create a new BasicClient. By default gzip is enabled.
+		BasicClient client = new ClientBuilder().name("sampleExampleClient").hosts(Constants.STREAM_HOST)
+				.endpoint(endpoint).authentication(auth).processor(new StringDelimitedProcessor(queue)).build();
 
-	    Observable<String> observable = Observable.from(queue);
-	    
+		// Establish a connection
+		client.connect();
 
-	    
-	    
-	    Observer<String> observe = new Observer<String> (){
-
-			@Override
-			public void onCompleted() {
+		// Do whatever needs to be done with messages
+		for (int msgRead = 0; msgRead < 100; msgRead++) {
+			if (client.isDone()) {
+				System.out.println("Client connection closed unexpectedly: " + client.getExitEvent().getMessage());
+				break;
 			}
 
-			@Override
-			public void onError(Throwable e) {
-				LOGGER.error(String.format("Error: %s", e));
+			try {
+				String msg = queue.poll(5, TimeUnit.SECONDS);
+				if (msg == null) {
+					System.out.println("Did not receive a message in 5 seconds");
+				} else {
+					System.out.println(msg);
+					//JSONObject jsonObj = new JSONObject("{\"phonetype\":\"N95\",\"cat\":\"WP\"}");
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 
-			@Override
-			public void onNext(String t) {
-				LOGGER.debug(t);
-			}
-	    	
-	    };
-	    
-	    observable.subscribe(observe);
-	    
-	    // Establish a connection
-	    client.connect();
-	   /*
-	    // Do whatever needs to be done with messages
-	    for (int msgRead = 0; msgRead < 1000; msgRead++) {
-	      if (client.isDone()) {
-	        System.out.println("Client connection closed unexpectedly: " + client.getExitEvent().getMessage());
-	        break;
-	      }
-
-	      Observable<String>
-	      
-	      String msg = queue.poll(5, TimeUnit.SECONDS);
-	      if (msg == null) {
-	        System.out.println("Did not receive a message in 5 seconds");
-	      } else {
-	        System.out.println(msg);
-	      }
-	    }
-*/
-	    
-	    try {
-			Thread.currentThread().sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			LOGGER.debug(e.getMessage());
 		}
-	    
-	    client.stop();
+
+		client.stop();
+
+		// Print some stats
+		System.out.printf("The client read %d messages!\n", client.getStatsTracker().getNumMessages());
 
 	}
 	
