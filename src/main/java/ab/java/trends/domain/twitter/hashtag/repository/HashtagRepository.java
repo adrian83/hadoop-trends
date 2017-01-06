30a6e4e1c2c538ffea9972e0f80f3cd91088aab8
adrian83 <adrianbrzoza1983@gmail.com>
2017-01-06 18:00:28 +0100

    first async functionality

--- src/main/java/ab/java/trends/domain/twitter/hashtag/repository/HashtagRepository.java
+++ src/main/java/ab/java/trends/domain/twitter/hashtag/repository/HashtagRepository.java
@@ -4,13 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.rethinkdb.RethinkDB;
+import com.rethinkdb.gen.ast.Table;
 import com.rethinkdb.net.Connection;
 
+import ab.java.trends.config.RethinkDBConfig;
 import ab.java.trends.domain.twitter.hashtag.domain.Hashtag;
+import ab.java.trends.domain.twitter.hashtag.domain.PopularHashtags;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,47 +24,54 @@ import org.slf4j.LoggerFactory;
 @Component
 public class HashtagRepository {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(HashtagRepository.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(HashtagRepository.class);
+	
+	private final static String DB_NAME = RethinkDBConfig.DB_NAME;
+	private final static String TABLE_NAME = RethinkDBConfig.HASHTAG_TABLE_NAME;
+	private final static String NAME_FIELD = RethinkDBConfig.HASHTAG_PK_NAME;
+	private final static String COUNT_FIELD = "count";
 
-    @Autowired
-    private Connection connection;
+	@Autowired
+	private Connection connection;
+	
+	private Table hashtags(){
+		return RethinkDB.r.db(DB_NAME).table(TABLE_NAME);
+	}
 
+	public void updateHashtag(String hashtag) {
 
-    public void updateHashtag(String hashtag) {
-    	    	
-        RethinkDB.r
-        .db("twitter_trends")
-        .table("hashtags")
-        .insert(RethinkDB.r.hashMap("name", hashtag).with("count", 1))
-        .optArg("conflict", 
-        		(id, old_doc, new_doc) -> new_doc.merge(RethinkDB.r.hashMap("count", old_doc.g("count").add(1))))
-        .run(connection);
-        
-        LOGGER.info("Hashtag {} updated", hashtag);
-    }
+		hashtags()
+				.insert(RethinkDB.r
+						.hashMap(NAME_FIELD, hashtag)
+						.with(COUNT_FIELD, 1))
+				.optArg("conflict", (id, old_doc, new_doc) -> new_doc.merge(RethinkDB.r.hashMap(COUNT_FIELD, old_doc.g(COUNT_FIELD).add(1))))
+				.run(connection);
 
-    public void updateHashtags(Stream<String> hashtags) {
-    	hashtags.forEach(hashtag -> updateHashtag(hashtag));
-    }
-    
-    public Stream<Hashtag> findMostPopular(int amount) {
-    	
-    	ArrayList<Object> result = RethinkDB.r
-        .db("twitter_trends")
-        .table("hashtags")
-        .orderBy(RethinkDB.r.desc("count"))
-        .limit(amount).run(connection);
-    	
-    	List<Hashtag> r = new ArrayList<>();
-    	
-    	for(Object m : result){
-    		@SuppressWarnings("unchecked")
-			Map<String, Object> map = (Map<String, Object>)m;
-    		r.add( new Hashtag(map.get("name").toString(), Integer.parseInt(map.get("count").toString())));
-    	}
-    	
-    	return r.stream();
-    	
-    }
+		LOGGER.info("Hashtag {} updated", hashtag);
+	}
+
+	public void updateHashtags(Stream<String> hashtags) {
+		hashtags.forEach(hashtag -> updateHashtag(hashtag));
+	}
+
+	public CompletionStage<PopularHashtags> findMostPopular(int amount) {
+
+		CompletionStage<PopularHashtags> ph = CompletableFuture
+				.supplyAsync(() -> {
+					
+			ArrayList<Map<String, Object>> result = hashtags()
+					.orderBy(RethinkDB.r.desc(COUNT_FIELD))
+					.limit(amount)
+					.run(connection);
+
+			Stream<Hashtag> str = result
+					.stream()
+					.map(o -> new Hashtag(o.get(NAME_FIELD).toString(), Integer.parseInt(o.get(COUNT_FIELD).toString())));
+
+			return new PopularHashtags(LocalDate.now(), str.collect(Collectors.toList()));
+		});
+
+		return ph;
+	}
 
 }
--- src/main/java/ab/java/trends/web/hashtag/HashtagController.java
+++ src/main/java/ab/java/trends/web/hashtag/HashtagController.java
@@ -1,14 +1,14 @@
 package ab.java.trends.web.hashtag;
 
-import java.util.stream.Stream;
+import java.util.concurrent.CompletionStage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ab.java.trends.domain.twitter.hashtag.domain.Hashtag;
 import ab.java.trends.domain.twitter.hashtag.repository.HashtagRepository;
-import reactor.core.publisher.Flux;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HashtagController {
@@ -17,13 +17,13 @@ public class HashtagController {
 	private HashtagRepository hashtagRepository;
 
 	@RequestMapping(value = "/hashtags/popular")
-	public Flux<HashtagDto> starters() {
+	public Mono<PopularHashtagsDto> starters() {
 		
-		Stream<Hashtag> tags = hashtagRepository.findMostPopular(10);
+		CompletionStage<PopularHashtagsDto> futurePopularTagsDto = hashtagRepository
+				.findMostPopular(10)
+				.thenApply(t -> t.map(ph -> new PopularHashtagsDto(ph.getTime(), ph.getHashtags())));
 		
-		Stream<HashtagDto> dts = tags.map(tag -> new HashtagDto(tag.getName().orElse("unknown"), tag.getCount().orElse(-1)));
-		
-		return Flux.fromStream(dts);//   .just(new HashtagDto("test", 111));
+		return Mono.fromFuture(futurePopularTagsDto.toCompletableFuture());
 	}
 	
 }
