cb98e3e63a838dbdeb365140f9b05873a1750261
adrian83 <adrianbrzoza1983@gmail.com>
2017-01-07 22:23:37 +0100

    moar rxjava

--- src/main/java/ab/java/trends/domain/twitter/hashtag/HashtagFinder.java
+++ src/main/java/ab/java/trends/domain/twitter/hashtag/HashtagFinder.java
@@ -1,33 +1,38 @@
 package ab.java.trends.domain.twitter.hashtag;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.Observable.OnSubscribe;
 
 @Component
 public class HashtagFinder {
-   private static final String HASHTAG_PATTERN_STR = "#[a-zA-Z0-9]{1,}";
-   private static final Pattern HASHTAG_PATTERN = Pattern.compile(HASHTAG_PATTERN_STR);
-   
-   public Stream<String> findHashtags(String text) {
-       if(StringUtils.isEmpty(text)) {
-           return Stream.empty();
-       }
-       
-       List<String> hashtags = new ArrayList<>();
-       
-       Matcher matcher = HASHTAG_PATTERN.matcher(text);
-       
-       while(matcher.find()) {
-           hashtags.add(text.substring(matcher.start(), matcher.end()));
-       }
+	private static final String HASHTAG_PATTERN_STR = "#[a-zA-Z0-9]{1,}";
+	private static final Pattern HASHTAG_PATTERN = Pattern.compile(HASHTAG_PATTERN_STR);
 
-       return hashtags.stream();
-   }
-   
+	public Observable<String> findHashtags(String text) {
+
+		return Observable.create(new OnSubscribe<String>() {
+
+			@Override
+			public void call(Subscriber<? super String> subscriber) {
+
+				if (StringUtils.isEmpty(text)) {
+					subscriber.onCompleted();
+				}
+
+				Matcher matcher = HASHTAG_PATTERN.matcher(text);
+
+				while (matcher.find()) {
+					subscriber.onNext(text.substring(matcher.start(), matcher.end()));
+				}
+				subscriber.onCompleted();
+			}
+		});
+	}
+
 }
--- src/main/java/ab/java/trends/domain/twitter/hashtag/subscriber/HashTagSubscriber.java
+++ src/main/java/ab/java/trends/domain/twitter/hashtag/subscriber/HashTagSubscriber.java
@@ -4,15 +4,20 @@ import ab.java.trends.domain.twitter.hashtag.HashtagFinder;
 import ab.java.trends.domain.twitter.hashtag.repository.HashtagRepository;
 
 import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import rx.Observable;
 import rx.Subscriber;
 import twitter4j.Status;
 
 @Component
 public class HashTagSubscriber extends Subscriber<Status> {
     
-    //private static final Logger LOGGER = LoggerFactory.getLogger(HashTagSubscriber.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(HashTagSubscriber.class);
     
     @Autowired
     private HashtagRepository hashtagRepository;
@@ -27,15 +32,19 @@ public class HashTagSubscriber extends Subscriber<Status> {
 
     @Override
     public void onError(Throwable e) {
-
+    	LOGGER.warn("Cannot read hashtag. Exception: {}", e);
     }
 
     @Override
     public void onNext(Status status) {
         
-        Stream<String> hashtags = hashtagFinder.findHashtags(status.getText());
+         Observable<String> hashtags = hashtagFinder.findHashtags(status.getText());
+         
+         hashtags.forEach(
+        		 hashtag -> hashtagRepository.updateHashtag(hashtag), 
+        		 throwable -> LOGGER.warn("Cannot update hashtag. Exception: {}", throwable));
     
-        hashtagRepository.updateHashtags(hashtags);
+
         
     }
     
