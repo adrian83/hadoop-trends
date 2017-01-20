a7024b3f2b4e40ec0e9e28423db80f56c6ba6ec3
Brzoza, Adrian <brzozaa@ryanair.com>
2017-01-20 14:05:00 +0100

    returning observable from controllers

--- src/main/java/ab/java/trends/domain/twitter/hashtag/repository/HashtagRepository.java
+++ src/main/java/ab/java/trends/domain/twitter/hashtag/repository/HashtagRepository.java
@@ -10,6 +10,10 @@ import com.rethinkdb.net.Connection;
 import ab.java.trends.config.RethinkDBConfig;
 import ab.java.trends.domain.twitter.hashtag.domain.Hashtag;
 import ab.java.trends.domain.twitter.hashtag.domain.PopularHashtags;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Observable.OnSubscribe;
+import twitter4j.Status;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -25,7 +29,7 @@ import org.slf4j.LoggerFactory;
 public class HashtagRepository {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HashtagRepository.class);
-	
+
 	private final static String DB_NAME = RethinkDBConfig.DB_NAME;
 	private final static String TABLE_NAME = RethinkDBConfig.HASHTAG_TABLE_NAME;
 	private final static String NAME_FIELD = RethinkDBConfig.HASHTAG_PK_NAME;
@@ -33,18 +37,17 @@ public class HashtagRepository {
 
 	@Autowired
 	private Connection connection;
-	
-	private Table hashtags(){
+
+	private Table hashtags() {
 		return RethinkDB.r.db(DB_NAME).table(TABLE_NAME);
 	}
 
 	public void updateHashtag(String hashtag) {
 
-		hashtags()
-				.insert(RethinkDB.r
-						.hashMap(NAME_FIELD, hashtag)
-						.with(COUNT_FIELD, 1))
-				.optArg("conflict", (id, old_doc, new_doc) -> new_doc.merge(RethinkDB.r.hashMap(COUNT_FIELD, old_doc.g(COUNT_FIELD).add(1))))
+		hashtags().insert(RethinkDB.r.hashMap(NAME_FIELD, hashtag).with(COUNT_FIELD, 1))
+				.optArg("conflict",
+						(id, old_doc, new_doc) -> new_doc
+								.merge(RethinkDB.r.hashMap(COUNT_FIELD, old_doc.g(COUNT_FIELD).add(1))))
 				.run(connection);
 
 		LOGGER.info("Hashtag {} updated", hashtag);
@@ -54,24 +57,28 @@ public class HashtagRepository {
 		hashtags.forEach(hashtag -> updateHashtag(hashtag));
 	}
 
-	public CompletionStage<PopularHashtags> findMostPopular(int amount) {
+	public Observable<Hashtag> findMostPopular(int amount) {
 
-		CompletionStage<PopularHashtags> ph = CompletableFuture
-				.supplyAsync(() -> {
-					
-			ArrayList<Map<String, Object>> result = hashtags()
-					.orderBy(RethinkDB.r.desc(COUNT_FIELD))
-					.limit(amount)
-					.run(connection);
+		return Observable.<Hashtag>create(new OnSubscribe<Hashtag>() {
 
-			Stream<Hashtag> str = result
-					.stream()
-					.map(o -> new Hashtag(o.get(NAME_FIELD).toString(), Integer.parseInt(o.get(COUNT_FIELD).toString())));
+			@Override
+			public void call(Subscriber<? super Hashtag> subscriber) {
+				try {
 
-			return new PopularHashtags(LocalDate.now(), str.collect(Collectors.toList()));
+					ArrayList<Map<String, Object>> result = hashtags().orderBy(RethinkDB.r.desc(COUNT_FIELD))
+							.limit(amount).run(connection);
+
+					for (Map<String, Object> map : result) {
+						subscriber.onNext(new Hashtag(map.get(NAME_FIELD).toString(),
+								Integer.parseInt(map.get(COUNT_FIELD).toString())));
+					}
+					subscriber.onCompleted();
+				} catch (Exception e) {
+					subscriber.onError(e);
+				}
+			}
+
 		});
-
-		return ph;
 	}
 
 }
--- src/main/java/ab/java/trends/web/hashtag/HashtagController.java
+++ src/main/java/ab/java/trends/web/hashtag/HashtagController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ab.java.trends.domain.twitter.hashtag.domain.Hashtag;
+import ab.java.trends.domain.twitter.hashtag.domain.PopularHashtags;
 import ab.java.trends.domain.twitter.hashtag.repository.HashtagRepository;
 
 import reactor.core.publisher.Mono;
+import rx.Observable;
 
 @RestController
 public class HashtagController {
@@ -16,14 +19,20 @@ public class HashtagController {
 	@Autowired
 	private HashtagRepository hashtagRepository;
 
+	@RequestMapping(value = "/test")
+	public Observable<HashtagDto> test() {
+		return Observable.just(new HashtagDto("test",2),new HashtagDto("test2",4));
+
+	}
+	
 	@RequestMapping(value = "/hashtags/popular")
-	public Mono<PopularHashtagsDto> starters() {
+	public Observable<HashtagDto> starters() {
 		
-		CompletionStage<PopularHashtagsDto> futurePopularTagsDto = hashtagRepository
-				.findMostPopular(10)
-				.thenApply(t -> t.map(ph -> new PopularHashtagsDto(ph.getTime(), ph.getHashtags())));
+		return hashtagRepository
+		.findMostPopular(10).map(h -> new HashtagDto(h.getName().orElse("asda"), h.getCount().orElse(-1)));
 		
-		return Mono.fromFuture(futurePopularTagsDto.toCompletableFuture());
 	}
 	
 }
+
+
