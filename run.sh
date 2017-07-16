fdd5e6386b7193ca4e8dbb2dcf11e0755db9bb4c
adrian83 <adrianbrzoza1983@gmail.com>
2017-07-16 19:32:51 +0200

    run.sh script updated

--- run.sh
+++ run.sh
@@ -6,8 +6,7 @@ usage() {
     Usage: $(basename $0) <command>
 
     run-docker            Starts Docker on Arch based GNU Linux.
-    run-rethink           Starts RethinkDB docker image.
-    run-mongo           Starts MongoDB docker image.
+    run-mongo             Starts MongoDB docker image.
     run-infra             Starts Docker and RethinkDB and Redis
 
 EOF
@@ -20,17 +19,10 @@ run-docker() {
 	set +e
 }
 
-run-rethink() {
-	set -e
-		docker run -p 28018:28015 -p 8082:8080 -v $PWD/rethinkdb:/data -d rethinkdb
-		echo "RethinkDB is listening on ports: 28018 and 8082. Data is stored inside 'rethinkdb' directory"
-	set +e
-}
-
 run-mongo() {
 	set -e
-		docker run -p 27017:27017 -v $PWD/mongodb:/data/db -d mongo:latest
-		echo "MongoDB is listening on ports: 27017. Data is stored inside 'mongodb' directory"
+		docker run -p 27021:27017 -v $PWD/mongodb:/infra/data/db -d mongo:latest
+		echo "MongoDB is listening on ports: 27021. Data is stored inside 'mongodb' directory"
 	set +e
 }
 
@@ -38,7 +30,6 @@ run-mongo() {
 run-infra() {
 	set -e
 		run-docker
-		run-rethink
 		run-mongo
 	set +e
 }
@@ -49,9 +40,6 @@ shift
 case "$CMD" in
 	run-docker)
 		run-docker
-	;;
-	run-rethink)
-		run-rethink
 	;;
 	run-mongo)
 		run-mongo
