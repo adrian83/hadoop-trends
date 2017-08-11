d6365133fc6f44471ea7807f9106966cb07fc7f9
adrian83 <adrianbrzoza1983@gmail.com>
2017-08-11 22:21:09 +0200

    logging updated (1)

--- src/main/java/ab/java/twittertrends/domain/twitter/hashtag/HashtagProcessor.java
+++ src/main/java/ab/java/twittertrends/domain/twitter/hashtag/HashtagProcessor.java
@@ -1,25 +1,25 @@
 package ab.java.twittertrends.domain.twitter.hashtag;
 
-
-
 import java.util.List;
 import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
-
 import javax.annotation.PostConstruct;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ab.java.twittertrends.domain.twitter.TwittsSource;
 import ab.java.twittertrends.domain.twitter.hashtag.repository.HashtagRepository;
+import twitter4j.Status;
+
 
 @Component
 public class HashtagProcessor {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(HashtagProcessor.class);
+	private static final Logger LOGGER = Logger.getLogger(HashtagProcessor.class.getSimpleName());
 
+	private static final int DEF_BUFFER_SIZE = 100;
+	
 	@Autowired
 	private TwittsSource twittsSource;
 	
@@ -32,25 +32,30 @@ public class HashtagProcessor {
 	
 	@PostConstruct
 	public void postCreate() {
-		LOGGER.debug("Starting processing hashtags");
-		
-		persistTwitts();
+		LOGGER.log(Level.INFO, "HashtagProcessor created");
+		persistHashtags();
 	}
 	
-	private void persistTwitts() {
+	private void persistHashtags() {
+		LOGGER.log(Level.INFO, "Starting persisting hashtags");
+		
 		twittsSource.twitts()
-		.map(s -> s.getText())
-        .flatMap(text -> hashtagFinder.findHashtags(text))
-        .buffer(200)
+		.map(Status::getText)
+        .flatMap(hashtagFinder::findHashtags)
+        .buffer(DEF_BUFFER_SIZE)
         .subscribe(hashtags -> {
+        	LOGGER.log(Level.INFO, String.format("Persisting %s hashtags", hashtags.size()));
+        	
         	List<Hashtag> tags = hashtags.stream()
         	.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
         	.entrySet()
-        	.stream().map(e -> ImmutableHashtag.builder()
+        	.stream()
+        	.map(e -> ImmutableHashtag.builder()
         			.name(e.getKey())
         			.count(e.getValue())
         			.build())
         	.collect(Collectors.toList());
+        	
         	mongoRepository.save(tags);
         	
         });
